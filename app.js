42c4af87b8f34bc04ca9db2f4b8a7a9eb734fbc1
Milo Vrach <morelo87@gmail.com>
2019-03-02 14:01:58 +0100

    Finish win, draw and lose functions.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const userScore = 0;
-const computerScore = 0;
+const smallUser = 'user'.fontsize(3).sub();
+const smallComp = 'comp'.fontsize(3).sub();
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const compScore_span = document.getElementById('comp-score');
 const scoreBoard_div = document.getElementById("score-div");
-const result_div = document.getElementsByClassName("result_div");
+const result_div_p = document.getElementById("result-div-p");
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors')
@@ -15,15 +17,32 @@ function getCompChoice(){
 }
 
 
-function win() {
+function firstLetterUpercase(letter){
+   return letter.charAt(0).toUpperCase() + letter.slice(1);
+}
+
+function win(userChoice, compChoice) {
+   userScore++;
+   userScore_span.innerHTML = userScore;
+   compScore_span.innerHTML = computerScore;
+   result_div_p.innerHTML = firstLetterUpercase(userChoice) + smallUser + " beats " + firstLetterUpercase(compChoice) + smallComp + ".You win!"   
+   console.log(userScore);
    console.log("User win");
 }
 
-function loose() {
+function loose(userChoice, compChoice) {
+   computerScore++;
+   userScore_span.innerHTML = userScore;
+   compScore_span.innerHTML = computerScore;
+   result_div_p.innerHTML = firstLetterUpercase(compChoice) + smallComp + " beats " + firstLetterUpercase(userChoice) + smallUser + ".You lose!"   
    console.log("User lose");
 }
 
-function draw(){
+function draw(userChoice, compChoice){
+   userScore_span.innerHTML = userScore;
+   compScore_span.innerHTML = computerScore;
+   result_div_p.innerHTML = firstLetterUpercase(compChoice) + smallComp + " equals " + firstLetterUpercase(userChoice) + smallUser + ".Draw!"
+ 
   console.log("It is a draw");
 }
 
@@ -34,17 +53,17 @@ function game(userChoice){
      case "paperrock":
      case "scissorspaper":
        
-       win();
+       win(userChoice, compChoice);
        break;
      case "rockpaper":
      case "paperscissors":
      case "scissorsirock":
-       loose()
+       loose(userChoice, compChoice)
        break;
      case "paperpaper":
      case "rockrock":
      case "scissorsscissors":
-       draw();
+       draw(userChoice, compChoice);
        break;
  }
 }
