7b50db755684817fba439a4a0f623c4390505f17
Milo Vrach <morelo87@gmail.com>
2019-03-02 11:36:14 +0100

    Add a main function

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("hello");
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const compScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.getElementsByClassName(".score-board");
+const result_div = document.getElementsByClassName(".result");
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors')
+
+
+main();
+
+function game(userChoice){
+   console.log(userChoice);
+}
+
+function main() {
+  rock_div.addEventListener('click', function(){
+     game('rock');
+  })
+
+  paper_div.addEventListener('click', function(){
+     game('paper');
+  })
+
+  scissors_div.addEventListener('click', function(){
+     game('scissors');
+  })
+}
